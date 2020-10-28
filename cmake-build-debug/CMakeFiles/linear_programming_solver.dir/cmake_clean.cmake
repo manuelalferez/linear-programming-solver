7d0c781524caa3852b2eddc6bfe85108bdb034de
manuelalferez <manuelalferezruiz@gmail.com>
2020-10-28 09:22:30 +0100

    Initial commit. Basic data file and file to save the Problem

---
+++ cmake-build-debug/CMakeFiles/linear_programming_solver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linear_programming_solver.dir/src/Problem.cpp.o"
+  "CMakeFiles/linear_programming_solver.dir/src/main.cpp.o"
+  "linear_programming_solver"
+  "linear_programming_solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linear_programming_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
